2d7f3c0a88a7af80a81145055a9273bf88b56fcd
xHayden <93haydcarp@csdecatur.net>
2019-11-21 22:30:48 -0500

    i made things harder for you

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -7,6 +7,11 @@ using System.Web.UI.WebControls;
 
 namespace TicketValley
 {
+    public class userDirectory
+    {
+        public string user = "hayden";
+        //I probably shouldn't put this here, but whatever.
+    }
     public partial class _Default : Page
     {
         protected void Page_Load(object sender, EventArgs e)
--- SelectTicketLocationTemplate.aspx.cs
+++ SelectTicketLocationTemplate.aspx.cs
@@ -9,6 +9,7 @@ namespace TicketValley
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
+        //my code is bad
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -17,7 +18,11 @@ namespace TicketValley
         protected void submitTicketsButton_Click(object sender, EventArgs e)
         {
             //Code for submitting tickets goes here
-            var path = "C:/Users/carte/source/repos/TicketValley/testing.txt";
+            var path = "";
+            userDirectory ahhidontremembercsharp = new userDirectory();
+            path = "C:/Users/" + ahhidontremembercsharp.user.TrimEnd(ahhidontremembercsharp.user[ahhidontremembercsharp.user.Length-1]) + "/source/repos/TicketValley/testing.txt";
+            //Thank you Carter for making this static to your directory. I fixed it.
+            //Also thank you Microsoft for making the name of your users the username minus the last char
 
             using (System.IO.StreamWriter file = new System.IO.StreamWriter(path))
             {
